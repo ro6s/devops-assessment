dbb56800d69e7cedc466157f9f575330137b968e
ro6s <74461595+ro6s@users.noreply.github.com>
2021-10-14 10:07:08 -0500

    Create app.py

---
+++ node1/app.py
@@ -0,0 +1,14 @@
+from flask import request, Flask
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hi. Node1 here.'
+
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port='5000')
